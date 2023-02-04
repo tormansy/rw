2dbfcb389fa9886832e4b0049e0f2b9eb712bbd8
tormansy <65613984+tormansy@users.noreply.github.com>
2023-02-04 21:12:21 +0300

    setup ts

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 const mode = process.env.NODE_ENV === 'production' ? 'production' : 'development';
 
 module.exports = {
-  entry: './src/scripts/index.js',
+  entry: './src/scripts/index.ts',
   mode,
   module: {
     rules: [
